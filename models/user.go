300cef51e324477e0d6336ddb86c7610282815e0
Jerome-afk <otienojerome7@gmail.com>
2025-07-10 14:36:12 +0300

    Updated database

--- models/user.go
+++ models/user.go
@@ -8,6 +8,13 @@ type User struct {
 	gorm.Model
 	Username     string `json:"username" gorm:"unique;not null"`
 	Email        string `json:"email" gorm:"unique;not null"`
-	PasswordHash string `json:"password_hash" gorm:"not null"`
+	Password     string `json:"password_hash" gorm:"not null"`
 	ProfileImage string `json:"profile_image" gorm:"default:'default.png'"`
+	SessionToken string `json:"session_token" gorm:"size:255;index"`
+}
+
+type AuthForm struct {
+	Username string `form:"username"`
+	Email	 string `form:"email"`
+	Password string `form:"password"`
 }
