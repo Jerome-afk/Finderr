19c09cb6f21b2669b60312158ae9fd10f050fd0a
Jerome-afk <otienojerome7@gmail.com>
2025-07-15 13:38:29 +0300

    Fix: Issue with poster display

--- models/media.go
+++ models/media.go
@@ -3,7 +3,7 @@ package models
 type MediaItem struct {
 	ID             int        `json:"id"`
 	Title          string     `json:"title"`
-	Poster         string     `json:"poster_path`
+	Poster         string     `json:"poster_path"`
 	CoverImage     string     `json:"cover_image"`
 	Rating         float64    `json:"vote_average"`
 	AverageScore   int        `json:"average_score"`
