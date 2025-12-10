3d8877fbcc84778121a3eb0ae09bbc7d5d705982
Jerome-afk <otienojerome7@gmail.com>
2025-12-10 14:20:16 +0300

    Very few

--- routes/routes.go
+++ routes/routes.go
@@ -3,5 +3,26 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func SetRoutes(app *fiber.App) {
-	
+	// Serve the static files
+	app.Static("/static", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("pages/index", fiber.Map{
+			"Title": "Home",
+		}, "layout/default")
+	})
+
+	// Authentication pages
+	auth := app.Group("/auth")
+	auth.Get("/login", func(c *fiber.Ctx) error {
+		return c.Render("auth/login", fiber.Map{
+			"Title": "Login",
+		}, "layout/auth")
+	})
+
+	// Normal pages
+	// norm := app.Group("/pages")
+
+	// Configuration pages
+	// conf := app.Group("/config")
 }
